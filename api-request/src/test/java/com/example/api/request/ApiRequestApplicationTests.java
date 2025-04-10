15cc250a5b31251739f559a2841b2d3ac6f424b2
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-04-10 14:46:35 +0545

    setup microservices

---
+++ api-request/src/test/java/com/example/api/request/ApiRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
