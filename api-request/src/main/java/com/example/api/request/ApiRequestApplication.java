514adbdd15677cf44e1d179fc768e9ee19e903f6
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-04-11 13:38:04 +0545

    feat:prac

--- api-request/src/main/java/com/example/api/request/ApiRequestApplication.java
+++ api-request/src/main/java/com/example/api/request/ApiRequestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiRequestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRequestApplication.class, args);
 	}
